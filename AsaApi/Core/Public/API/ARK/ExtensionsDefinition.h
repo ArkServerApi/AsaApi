b6675d2c44c1aeb4cc64d34cb443c4e1710d166a
Riley S. <substitute@pocketdevs.org>
2024-10-11 21:52:55 -0400

    Clean up Extensions Definitions

--- AsaApi/Core/Public/API/ARK/ExtensionsDefinition.h
+++ AsaApi/Core/Public/API/ARK/ExtensionsDefinition.h
@@ -1,45 +1,41 @@
+// ReSharper disable CppInconsistentNaming
 #pragma once
 
-#include "../../IApiUtils.h"
+#define STRINGIFY(a) #a
+#define Name_Persist(name) _Pragma( STRINGIFY( comment(linker, "/Export:AsaApi_" ## name ## "=" __FUNCDNAME__) ))
 
-// Actor extensions
-
-FORCEINLINE FVector ActorExtensions::GetActorForwardVector()
+DLLEXPORT FORCEINLINE FString GetSteamName(AController& player_controller)
 {
-    AActor* _this = static_cast<AActor*>(this);
-    USceneComponent* RootComponent = _this->RootComponentField().Get();
-    if (RootComponent)
-    {
-        return RootComponent->ComponentToWorldField().GetUnitAxis(EAxis::X);
-    }
-
-    //return FVector::ZeroVector;
-    return FVector(0, 0, 0);
+    Name_Persist("IApiUtils_GetSteamName_0");
+    if (const auto& player_state = player_controller.PlayerStateField())
+        return player_state->PlayerNamePrivateField();
+    return "";
 }
 
-FORCEINLINE bool ActorExtensions::IsA(UClass* SomeBase)
+// Actor extensions
+FORCEINLINE FVector ActorExtensions::GetActorForwardVector()
 {
-    return static_cast<AActor*>(this)->ClassPrivateField()->IsChildOf(SomeBase);
+    if (USceneComponent* root_component = std::bit_cast<AActor*>(this)->RootComponentField().Get())
+        return root_component->ComponentToWorldField().GetUnitAxis(EAxis::X);
+    return FVector::ZeroVector;
+}
+
+FORCEINLINE bool ActorExtensions::IsA(const UClass* some_base)
+{
+    return std::bit_cast<AActor*>(this)->ClassPrivateField()->IsChildOf(some_base);
 }
 
 FORCEINLINE FVector ActorExtensions::GetLocation()
 {
-    AActor* _this = static_cast<AActor*>(this);
-    auto* root = _this->RootComponentField().Get();
-    if (root)
-    {
-        UE::Math::TTransform<double> transform = root->ComponentToWorldField();
-        return transform.GetLocation();
-    }
-    return FVector();
+	if (const auto& root = std::bit_cast<AActor*>(this)->RootComponentField())
+        return root->ComponentToWorldField().GetLocation();
+    return FVector::ZeroVector;
 }
 
 // Player Controller Extensions
 
 FORCEINLINE FString PlayerControllerExtensions::GetEOSId()
 {
-    AShooterPlayerController* _this = static_cast<AShooterPlayerController*>(this);
-    FString eos_id = "";
-    _this->GetUniqueNetIdAsString(&eos_id);
+    FString eos_id = *std::bit_cast<AShooterPlayerController*>(this)->GetUniqueNetIdAsString(&eos_id);
     return eos_id;
 }
